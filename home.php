a921d91d32fae48fec57ea1bca6ec913c64d6329
aiarnob23 <aiarnob23@gmail.com>
2024-04-23 19:59:17 +0600

    mini project for dbms lab

---
+++ home.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- playcdn links of tailwindcss -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <!-- link of raw css -->
+    <link rel="stylesheet" href="./styles/style.css">
+    <!-- playcdn links of daisyui -->
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.10.2/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <title>Document</title>
+</head>
+
+<body>
+<!----------------Header Section -------------->
+<section class="container relative my-4 mx-auto">
+ <div class="hero h-[600px] rounded-lg" style="background-image: url('./src/images/header.jpg');">
+  <div class="hero-overlay bg-opacity-50"></div>
+  <div class="hero-content text-center text-neutral-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Backbencher Travel Agency</h1>
+    </div>
+  </div>
+</div>
+</section>
+
+<!-- ----------main body------ -->
+<main>
+  <h1>Our Packages</h1>
+</main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="./scripts/script.js"></script>
+</body>
+</html>
